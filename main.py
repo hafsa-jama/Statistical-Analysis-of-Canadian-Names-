995fc4ef44e95926623302e2849464fa573c8bd6
Tanveer Sadat <tsadat@fred.socs.uoguelph.ca>
2023-03-10 09:29:45 -0500

    Added to main file

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import getopt
+import csv
+import pandas as pd
+
+def main (argv):
+
+    print("\nWelcome to Team Cheetah's CIS2250 Team Project\n")
+
+if __name__ == "__main__":
+    main ( sys.argv[1:] )
