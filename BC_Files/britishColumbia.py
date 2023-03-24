5349dc79dc12aaad4ff02c52416f5539fb78e0fc
Hafsa Jama <jamah@fred.socs.uoguelph.ca>
2023-03-24 09:41:42 -0400

    done

--- BC_Files/britishColumbia.py
+++ BC_Files/britishColumbia.py
@@ -1,80 +1,96 @@
-# To Run: python3 ./britishColumbia.py BritishColombiaBoys_1922_2021.csv BritishColombiaGirls_1922_2021.csv
-# Format: year/name/frequency/rank
+# To Run: chmod 777 EVERYTHING
+#         python3 ./britishColumbia.py BritishColombiaBoys_1922_2021.csv BritishColombiaGirls_1922_2021.csv
+# Format: year,name,frequency,rank
 
 import csv
-import pandas as pd
 
-def main(argv, fn, index):
-    
-   # initalizes dictonaries to store data read from CSV file
-    people = {}
-    names = {}
-    years = {}
-    
+# define a class record to store information about each name
+class Record:
+    headers = ('name', 'years', 'rank')
 
-    # initalizes variables
+    # degine the constructor for the class
+    def __init__(self, name):
+        self.name = name
+        self.years = {} 
+        self.rank = {} 
+
+    # define a method to update the rank for a specific year
+    def update_record_rank(self, year, rank):
+        self.rank[year] = rank
+
+    # define a method to get the years for which there is data for the name
+    def get_record_years(self):
+        return self.years
+
+    # define a method to get the rank for a specific year
+    def get_record_rank(self, year):
+        return self.rank[year]
+
+# define a function to sort the frequency of a name for a specific year
+def sort_frequency(record, year):
+    years = record.years
+    if year in years:
+        return (-int(years[year]), record.name, ord(record.name[0]))
+    else:
+        return (0, record.name, ord(record.name[0]))
+
+# main function
+def main(input_file, output_file):
+
+    # initalize the varibales for start and end years for the data
     startYear = 1922
-    endYear = 2022
-    count = 0
-    fileName = argv[index] # assigns the value of the command-line argument at the specified index to a variable
+    endYear = 2021
 
+    # create a dictionary to store the index of each record by name
+    records = []
 
-    # opens the csv file using encoding
-    with open(fileName, encoding='windows-1252') as csvFile:
-        # reads the csv file using csv reader 
-        reader = csv.reader(csvFile)
-        # iterate through each row in csv file
+    # create a dictionary to store the index od each record by name
+    record_names = {}
+
+    record_count = 0 # counter for number of records
+
+    # opens the input file
+    with open(input_file, encoding="windows-1252") as csvfile:
+        # iterate through each row in the CSV file
+        reader = csv.reader(csvfile)
         for row in reader:
-            # if the first value of the row is not "Name"
             if row[0] != "Name":
-                # reassigns the first value of the row
-                name = row[0]
-                # new dictionary is created to store the data for the person
-                people[name] = {}
-                # iterate through the range of years
+                # create a new Record object for the name
+                record = Record(row[0])
+
+                # store the data for each year for the record
+                years = record.years
                 for year in range(startYear, endYear):
-                    # retrieve the frequency value for the current year and person 
-                    frequency = row[year - startYear + 2].replace(',', '')
-                    # convert the frequency value to an integer and store it in the dictionary
-                    people[name][year] = int(frequency)
-                # store the count of the person's name in the names dictionary
-                names[name] = count
-                count += 1 # increments count by 1
+                    years[year] = row[year - startYear + 2].replace(',', '') 
+                records.append(record) # adds the record to the list of records
+                record_names[row[0]] = record_count # store the index of the record by name in the dictionary
+                record_count += 1 # increment the counter for the number of records
 
-    #  ranks each person in the people dictionary based on their frequency in each year #
-
-    # iterates through each year
+    # iterate through each year in the data
     for year in range(startYear, endYear):
-      # gets list of people's names sorted by their frequency in the given year
-        sortedFrequencyPeople = sorted(
-        people.keys(),
-         key=lambda x: people[x].get(year, 0), # sort by frequency or 0 if missing
-         reverse=True, # sorts in descending order
-)
-        # assigns the rank to each person based on their frequency in a given year
-        for j, person in enumerate(sortedFrequencyPeople, 1):
-            people[person]["rank_{}".format(year)] = j
+        # sort the records by frequency for the year 
+        sorted_frequency_records = sorted(records, key=lambda x: sort_frequency(x, year))
 
-    # writes a new output to a csv file
-    with open(fn, "w") as file:
+        # updates the rank for each record for the year
+        for j, record in enumerate(sorted_frequency_records, 1):
+            records[record_names[record.name]].update_record_rank(year, j)
+
+    # opens the output fileand uses the CSV writer to write data to the file
+    with open(output_file, 'w', newline='') as file:
         writer = csv.writer(file)
-       # iterates through each year between startYear and endYear
         for year in range(startYear, endYear):
             # creates a list of tuples with each tuple containing the name and rank for each person for the given year
-            rankedList = [(name, people[name]["rank_{}".format(year)]) for name in people.keys() if "rank_{}".format(year) in people[name]]
+            rankedList = [(person.name, person.get_record_rank(year)) for person in records if year in person.rank]
             # sorts the rankedList based on rank
-            rankedList.sort(key=lambda x: people[x[0]].get(year, 0), reverse=True)
+            rankedList.sort(key=lambda x: x[1])
 
-            # iterates through each person in the rankedList and writes their informatio to the CSV file
+            # iterates through each person in the rankedList and writes their information to the CSV file
             for person in rankedList:
-                writer.writerow([year, person[0].title(), names[person[0]], people[person[0]]["rank_{}".format(year)]])
+                years = records[record_names[person[0]]].get_record_years()
+                writer.writerow([person[1], year, person[0].upper().replace("_", " "), years[year]])
 
 
 # output files
 if __name__ == "__main__":
-    import sys
-    main(sys.argv, "BritishColumbia_M.csv", 1)
-    main(sys.argv, "BritishColumbia_F.csv", 2)
-
-    
-
+    main("BritishColombiaBoys_1922_2021.csv", "BritishColumbia_M.csv")
+    main("BritishColombiaGirls_1922_2021.csv", "BritishColumbia_F.csv")
