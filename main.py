98c384bcac86ce3f73b2ac6aef4531a73488396d
Tanveer Sadat <tsadat@fred.socs.uoguelph.ca>
2023-03-31 10:16:33 -0400

    updated main

--- main.py
+++ main.py
@@ -6,39 +6,18 @@ import getopt
 import csv
 import pandas as pd
 
+from graphs import *
+
+
 def main (argv):
-  if len(argv)<9:
-    print("Usage : ./names.py -f1 <file input 1> -f2 <file input 2> -f3 <file input 3> -f4 <file input 4> -f5 <file input 5> -f6 <file input 6> -f7 <file input 7> -f8 <file input 8>")
-    sys.exit(2)
-  try: # to catch any errors that occur 
-    (opts, args) = getopt.getopt (argv,"f1:f2:f3:f4:f5:f6:f7:f8")
-  except getopt.GetoptError: #if an error occurs it prints this and exits 
-        print ("Usage : ./names.py -f1 <file input 1> -f2 <file input 2> -f3 <file input 3> -f4 <file input 4> -f5 <file input 5> -f6 <file input 6> -f7 <file input 7> -f8 <file input 8>")
-        sys.exit(2)
-  for opt, arg in opts:
-    if opt == '-h': ## this is used as an instruction 
-            print ( "Usage: ./analysisNames.py -i <input file name> -n <name>" )
-            sys.exit()
-    elif opt in ( "-f1", "--file input 1 "):
-            fileName_1 = arg
-    elif opt in ( "-f2", "--file input 2 "):
-            fileName_2 = arg
-    elif opt in ( "-f3", "--file input 3 "):
-            fileName_3 = arg
-    elif opt in ( "-f4", "--file input 4 "):
-            fileName_4 = arg
-    elif opt in ( "-f5", "--file input 5 "):
-            fileName_5 = arg
-    elif opt in ( "-f6", "--file input 6 "):
-            fileName_6 = arg
-    elif opt in ( "-f7", "--file input 7 "):
-            fileName_7 = arg
-    elif opt in ( "-f8", "--file input 8 "):
-            fileName_8 = arg
-      
-    def read_my_file(name_file,file_years,file_names,file_count,file_ranks):#function to read files 
+
+    # function that will from the files and store in lists      
+    def read_my_file(name_file,file_years,file_names,file_count,file_ranks):
        with open ( name_file ) as csvDataFile:
         csvReader = csv.reader(csvDataFile)
+
+        # skips the header row
+        next (csvReader)
         for row in csvReader:
           file_years.append(int(row[0]))
           file_names.append(row[1])
@@ -86,36 +65,153 @@ def main (argv):
     NewBrunswick_years_F = []
     NewBrunswick_count_F = []
 
+    fileName_1 = "alberta_M.csv"
+    fileName_2 = "alberta_F.csv"
+    fileName_3 = "BritishColumbia_M.csv"
+    fileName_4 = "BritishColumbia_F.csv"
+    fileName_5 = "NewBrunswick_M.csv"
+    fileName_6 = "NewBrunswick_F.csv"
+    fileName_7 = "NovaScotia_M.csv"
+    fileName_8 = "NovaScotia_F.csv"
+
+    
     read_my_file(fileName_1,Alberta_years_M,Alberta_names_M,Alberta_count_M,Alberta_ranks_M)
     read_my_file(fileName_2,Alberta_years_F,Alberta_names_F,Alberta_count_F,Alberta_ranks_F)
-    read_my_file(fileName_3,BC_years_M,BC_names_M,BC_count_M,BC_ranks_M)
-    read_my_file(fileName_4,BC_years_F,BC_names_F,BC_count_F,BC_ranks_F)
+    #read_my_file(fileName_3,BC_years_M,BC_names_M,BC_count_M,BC_ranks_M)
+    #read_my_file(fileName_4,BC_years_F,BC_names_F,BC_count_F,BC_ranks_F)
     read_my_file(fileName_5, NewBrunswick_years_M, NewBrunswick_names_M, NewBrunswick_count_M, NewBrunswick_ranks_M)
     read_my_file(fileName_6,NewBrunswick_years_F, NewBrunswick_names_F, NewBrunswick_count_F, NewBrunswick_ranks_F)  
     read_my_file(fileName_7,NovaScotia_years_M,NovaScotia_names_M,NovaScotia_count_M,NovaScotia_ranks_M)
     read_my_file(fileName_8,NovaScotia_years_F,NovaScotia_names_F,NovaScotia_count_F,NovaScotia_ranks_F)
-print("\nWelcome to Team Cheetah's CIS2250 Team Project\n")
-user_input=0
-while user_input==0:
-  print("Welcome to the menu")
-  print("1-Check if the name is present in a province\n")
-  print("2-Displays shortest name in each province\n")
-  print("3-Displays most frequently repeated name in a year\n")
-  print("4-Enter this to leave")
-  user_input = int(input("Enter your choice wisely :"))
-  if user_input==1:
-    print("choice 1")
-  
-  elif user_input==2:
-    print("choice 2")
-  elif user_input==3:
-    print("choice 3")
+
     
-  elif user_input==4:
-    print("hope you enjoyed")
-    break
-  else:
-    print("Please choose again")
+    print("\nWelcome to Team Cheetah's CIS2250 Team Project\n")
+    user_input = 0
+    while user_input == 0:
+        print("\n\n Welcome To The Menu \n\n")
+        print("1.   Graph 1 - Creates a line graph of the rank of a given name over time")
+        print("2.   Function 2 - Displays shortest name in each province")
+        print("3.   Function 3 - Displays most frequently repeated name in a year")
+        print("4.   Exit\n")
+
+        try:
+            user_input = int(input("Please enter in your choice: "))
+
+            if user_input == 1:
+                province = 0
+                while province == 0:
+                    provinceMenu()
+                    try:
+                        province = int(input("Enter province: "))
+
+                        if province == 1:
+                            gender = 0
+                            while gender == 0:
+                                genderMenu()
+                                try:
+                                    gender = int(input("Enter gender: "))
+
+                                    if gender == 1:
+                                        username = input("Enter a name: ")
+                                        nameOverTime(username, Alberta_years_M, Alberta_names_M, Alberta_ranks_M)
+
+                                    elif gender == 2:
+                                        username = input("Enter a name: ")
+                                        nameOverTime(username, Alberta_years_F, Alberta_names_F, Alberta_ranks_F)
+                            
+
+                                except ValueError:
+                                    print("Error. Please input a valid integer option.")
+                                    gender = 0
+
+                        if province == 2:
+                            gender = 0
+                            while gender == 0:
+                                genderMenu()
+                                try:
+                                    gender = int(input("Enter gender: "))
+
+                                    if gender == 1:
+                                        username = input("Enter a name: ")
+                                        #nameOverTime(username, BC_years_M, BC_names_M, BC_ranks_M)
+
+                                    elif gender == 2:
+                                        username = input("Enter a name: ")
+                                        #nameOverTime(username, BC_years_F, BC_names_F, BC_ranks_F)
+                            
+
+                                except ValueError:
+                                    print("Error. Please input a valid integer option.")
+                                    gender = 0
+
+                        if province == 3:
+                            gender = 0
+                            while gender == 0:
+                                genderMenu()
+                                try:
+                                    gender = int(input("Enter gender: "))
+
+                                    if gender == 1:
+                                        username = input("Enter a name: ")
+                                        nameOverTime(username, NewBrunswick_years_M, NewBrunswick_names_M, NewBrunswick_ranks_M)
+
+                                    elif gender == 2:
+                                        username = input("Enter a name: ")
+                                        nameOverTime(username, NewBrunswick_years_F, NewBrunswick_names_F, NewBrunswick_ranks_F)
+                            
+
+                                except ValueError:
+                                    print("Error. Please input a valid integer option.")
+                                    gender = 0
+
+                        if province == 4:
+                            gender = 0
+                            while gender == 0:
+                                genderMenu()
+                                try:
+                                    gender = int(input("Enter gender: "))
+
+                                    if gender == 1:
+                                        username = input("Enter a name: ")
+                                        nameOverTime(username, NovaScotia_years_M, NovaScotia_names_M, NovaScotia_ranks_M)
+
+                                    elif gender == 2:
+                                        username = input("Enter a name: ")
+                                        nameOverTime(username, NovaScotia_years_F, NovaScotia_names_F, NovaScotia_ranks_F)
+                            
+
+                                except ValueError:
+                                    print("Error. Please input a valid integer option.")
+                                    gender = 0
+                
+
+                    except ValueError:
+                        print("Error. Please input a valid integer option.")
+                        province = 0
+
+                
+        ## calls function ##
+
+            elif user_input == 2:
+                name = input("Please enter in a name: ")
+                ## calls function ##
+
+            elif user_input == 3:
+                print("choice 3")
+                ## calls function ## 
+
+            elif user_input == 4:
+                print("Exiting from menu...")
+                break
+
+            else:
+                print("Error. Please input a valid option.")
+                user_input = 0
+        
+        except ValueError:
+            print("Error. Please input a valid integer option.")
+            user_input = 0
+
 
 
 if __name__ == "__main__":
--- originalFiles/NovaScotia.py
+++ originalFiles/NovaScotia.py
@@ -22,7 +22,7 @@ def main ( argv ):
             sys.exit()
         elif opt in ( "-i", "--input"):
             inputFileName = arg
-    outputFileName = "NovaScotia_F.csv"
+    outputFileName = "NovaScotia_M.csv"
 
     names    = []
     count  = []
@@ -34,7 +34,7 @@ def main ( argv ):
         next ( csvDataFile ) 
         csvReader = csv.reader(csvDataFile, delimiter=',')
         for row in csvReader:
-            if (row[1]) == 'F' :
+            if (row[1]) == 'M' :
                 years.append(int(row[0]))
 
                 tempName = row[2].strip()
@@ -45,17 +45,19 @@ def main ( argv ):
                 count.append(int(row[3]))
 
                 total = total + 1
-                ranks.append(total)
+                #ranks.append(total)
 
         if total > 0 :
             people = {'Year':years,'Name':names,'Count':count}
             people_df = pd.DataFrame(people)
-            people_df.sort_values(["Count","Name"], axis = 0, ascending=[False,True], inplace=True)
+            people_df.sort_values(["Year","Count","Name"], axis = 0, ascending=[True,False,True], inplace=True)
 
-            rankedPeople_df = people_df.assign(Rank=ranks)
-            rankedPeople_df = rankedPeople_df.loc[:, ["Year", "Name", "Count", "Rank"]]
+            people_df['Rank'] = people_df.groupby('Year')['Count'].rank(ascending = False, method = 'dense').astype(int)
 
-            rankedPeople_df.to_csv(outputFileName, sep=',', index=False, encoding='utf-8')
+            #rankedPeople_df = people_df.assign(Rank=ranks)
+            people_df = people_df.loc[:, ["Year", "Name", "Count", "Rank"]]
+
+            people_df.to_csv(outputFileName, sep=',', index=False, encoding='utf-8')
 
 if __name__ == "__main__":
     main ( sys.argv[1:] )
